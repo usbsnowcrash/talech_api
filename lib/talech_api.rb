bee0070c7408539faa5d5aef966e44decee491db
usbsnowcrash <jeff@debug.ninja>
2018-07-12 15:14:16 -0400

    initial commit

---
+++ lib/talech_api.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require 'talech_api/version'
+
+module TalechApi
+  # Your code goes here...
+end
---
+++ spec/talech_api_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe TalechApi do
+  it 'has a version number' do
+    expect(TalechApi::VERSION).not_to be nil
+  end
+
+  it 'does something useful' do
+    expect(false).to eq(true)
+  end
+end
